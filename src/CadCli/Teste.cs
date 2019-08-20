8f7b889fb21e1afdf782588c3ecae73a2a0c2b1b
fabiano nalin <nalin@fansoft.com.br>
2019-08-20 01:03:56 +0000

    Aula 02

---
+++ src/CadCli/Teste.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace CadCli
+{
+    //[Controller]
+    //public class Teste
+
+
+
+    // public class TesteController
+    public class Teste : Controller
+    {
+
+        public string Ping()
+        {
+            return "Pong";
+        }
+
+    }
+}
